e3e336ab792755b0b1eb405367141cfc014e392a
João Victor Chaves <joao.victor.chaves159@gmail.com>
2020-09-29 15:20:29 -0300

    Correção Session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
+
     if user && user.authenticate(params[:session][:password])
       session[:id_user] = user.id
       sign_in(user)
