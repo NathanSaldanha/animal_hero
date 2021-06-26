17b87606da9677e52ee632b013fc0591d4a0ce67
andre-dan <andrerndc@gmail.com>
2021-06-26 14:23:03 -0300

    Criado funcionalidade editar usuario

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[edit update]
+
   def new
     @user = User.new
   end
@@ -13,8 +15,28 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+  
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+         format.html { redirect_to root_url, notice: 'Usuário atualizado com sucesso' }
+         format.json { render :show, status: :ok, location: @user }
+      else   
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
+  
     def user_params
       params.require(:user).permit(:email,:name,:password,:password_confirmation)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
     end
 end
