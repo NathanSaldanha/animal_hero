dfaef496231b7d18b5d651acedbc47ac88973cdb
janevieira <janevieirapinto@gmail.com>
2021-06-29 06:55:13 -0300

    Correção do Editar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,6 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:email,:name,:password,:password_confirmation)
+      params.require(:user).permit(:email,:name,:password,:edit,:password_confirmation)
     end
 end
