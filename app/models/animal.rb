4849b8905ee7aa8bc07798b926cfbd44a78790e0
Almerinda Rodrigues <melrylimace@gmail.com>
2022-01-31 21:30:52 -0300

    Adicionado Link de Lista de Animais

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -5,9 +5,9 @@ class Animal < ApplicationRecord
     if (params.keys.include?('animal'))
       if params[:animal][:nome].present?
         Animal.where("nome like '%#{params[:animal][:nome]}%'")
-      else
-        Animal.all  
       end  
+    else
+      Animal.all    
     end
   end  
 end
